2d97bf2f2185071e064bdca9ba0a142b54a250c6
Ilia Green <tim2015web@gmail.com>
2024-01-24 09:35:11 +0300

    micro fix for bars

--- src/components/header/controlsBar/ControlsBar.jsx
+++ src/components/header/controlsBar/ControlsBar.jsx
@@ -3,11 +3,18 @@ import styles from './ControlsBar.module.scss';
 export function ControlsBar() {
   return (
     <div className={styles.controlsBar}>
+
       <div className={styles.controlsBar__arrows}>
         <button id='slidePrev' />
         <button id='slideNext' />
       </div>
-      <div id='slidePaginat' className={styles.controlsBar__numbers}></div>
+
+      <div id='slidePaginat' className={styles.controlsBar__numbers}>
+        <span>01</span>
+        /
+        <span>04</span>
+      </div>
+
     </div>
   );
 };
